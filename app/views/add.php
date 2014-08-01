b3fce75bcce5962c79e0c44b5071a7441a75d31d
Nataly <nataly77777@ukr.net>
2014-08-01 17:36:36 +0300

    commit8

---
+++ app/views/add.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title></title>
+
+  
+    <link href="/assets/bootstrap/css/bootstrap.css" rel="stylesheet">
+    <link href="/mycss/style.css" rel="stylesheet" type="text/css">
+    <script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
+    <script src="/assets/bootstrap/js/bootstrap.js"></script>
+
+    
+  </head>
+
+  <body>
+
+    <div class="container">
+      <div class="header">
+        <ul class="nav nav-pills pull-right">
+          <li class="active"><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+        <h3 class="text-muted">Project name</h3>
+      </div>
+      <form class="form-horizontal" method="post" action="/index.php">
+       <input type="text" class="form-control" placeholder="Название списка" name="title">
+       </br>
+       <textarea class="form-control" rows="3" placeholder="Описание списка" name="data"></textarea>
+       </br>
+        <button type="submit" class="btn btn-default">Создать список дел</button>
+      </form>
+    </div>
+    </br>
+  </div>
+</form>
+
+      
+      <div class="footer">
+        <p>&copy; Company 2014</p>
+      </div>
+
+    </div> <!-- /container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+  </body>
+</html>
